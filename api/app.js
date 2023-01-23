e631461b891dc541142847c29ab3eecf4d527a0c
rachnakafle <rachanakafle99@gmail.com>
2023-01-23 17:04:09 +0545

    Working on db connection

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const {mongoose} = require('./db/mongoose')
-//const schema = mongoose.ListSchema
+const schema = mongoose.Schema
 
 const bodyParser = require('body-parser');
 
